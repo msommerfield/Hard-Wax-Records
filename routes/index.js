197e2b9bff856b64ca4d306b05ffc7155ca8ff03
Mandy <mandysommerfield@gmail.com>
2019-03-08 11:39:48 -0500

    rearranged routes and the page doesn't break anymore

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,17 @@ const router = express.Router({mergeParams: true})
 
 const vinylController = require('../controllers/vinylController.js')
 const reviewController = require('../controllers/reviewController.js')
-// const ratingController = require('../controllers/ratingController.js')
+const ratingController = require('../controllers/ratingController.js')
+
+//Rating Routes
+router.post('/vinyls/:vinylId/ratings', ratingController.create)
+router.get('/vinyls/:vinylId/ratings/:ratingId', ratingController.show)
+router.delete('/vinyls/:vinylId/ratings/:ratingId', ratingController.delete)
+
+// Review Routes
+router.post('/vinyls/:vinylId/reviews', reviewController.create)
+router.get('/vinyls/:vinylId/reviews/:reviewId', reviewController.show)
+router.delete('/vinyls/:vinylId/reviews/:reviewId', reviewController.delete)
 
 //Vinyl Routes
 router.get('/', vinylController.index)
@@ -16,19 +26,5 @@ router.get('/:vinylId', vinylController.show)
 router.get('/:vinylId/edit', vinylController.edit)
 router.put('/:vinylId', vinylController.update)
 router.delete('/:vinylId', vinylController.delete)
-//merge
-
-// Review Routes
-// router.get('/vinyls/:vinylId/reviews/new', reviewController.new)
-router.post('/vinyls/:vinylId/reviews', reviewController.create)
-router.get('/vinyls/:vinylId/reviews/:reviewId', reviewController.show)
-// router.get('/vinyls/:vinylId/reviews/:reviewId/edit', reviewController.edit)
-// router.put('/vinyls/:vinylId/reviews/:reviewId', reviewController.update)
-router.delete('/vinyls/:vinylId/reviews/:reviewId', reviewController.delete)
-
-//Rating Routes
-// router.post('/vinyls/:vinylId/ratings', ratingController.create)
-// router.get('/vinyls/:vinylId/ratings/:ratingId', ratingController.show)
-// router.delete('/vinyls/:vinylId/ratings/:ratingId', ratingController.delete)
 
 module.exports = router
